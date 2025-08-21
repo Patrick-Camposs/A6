c5ebe8a91c9cb608b25d6cd442df752a9dce38de
Patrick-Camposs <patrickcamposRD@gmail.com>
2025-08-21 16:55:57 -0300

    Add files via upload

---
+++ BACK-END_T6/Quiz.tentativa2/resultado.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Resultado do Quiz</title>
+</head>
+<body>
+    <?php
+    // Verifica se o nome e as respostas foram enviadas via formulário
+    if(isset($_POST['nome']) && isset($_POST['resposta1']) && isset($_POST['resposta2']) && isset($_POST['resposta3']) && isset($_POST['resposta4']) && isset($_POST['resposta5'])) {
+        $nome = $_POST['nome'];
+        $resposta1 = strtolower($_POST['resposta1']);
+        $resposta2 = strtolower($_POST['resposta2']);
+        $resposta3 = strtolower($_POST['resposta3']);
+        $resposta4 = strtolower($_POST['resposta4']);
+        $resposta5 = strtolower($_POST['resposta5']);
+
+        // Verifica as respostas
+       $acertos = 0;
+
+         if ($resposta1 === "brasilia") {
+             $acertos++;
+         }
+         if ($resposta2 === "cervantes") {
+             $acertos++;
+         }
+         if ($resposta3 === "oito") {
+             $acertos++;
+         } 
+         if ($resposta4 === "dois" || $resposta4 === "2") {
+             $acertos++;
+         }
+         if ($resposta5 === "flamengo") {
+             $acertos++;
+         }
+
+        // Exibe o resultado
+        echo "<h2>Resultado do Quiz</h2>";
+        echo "<p>Olá, $nome!</p>";
+        echo "<p>Você acertou $acertos de 5 perguntas.</p>";
+
+        // Depuração
+        echo "<pre>";
+        echo "Resposta 1: $resposta1\n";
+        echo "Resposta 2: $resposta2\n";
+        echo "Resposta 3: $resposta3\n";
+        echo "Resposta 4: $resposta4\n";
+        echo "resposta 5: $resposta5\n";
+        echo "</pre>";
+
+        // Botão para resetar
+        echo '<form action="index.php" method="post">';
+        echo '<button type="submit">Resetar Quiz</button>';
+        echo '</form>';
+    } else {
+        // Se as respostas não foram enviadas, redireciona para a página inicial
+        header("Location: index.php");
+        exit();
+    }
+    ?>
+</body>
+</html>
